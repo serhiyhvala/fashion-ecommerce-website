c010be86acde742f78f6ad9fe14ae0da371789e6
Your Name <you@example.com>
2022-03-31 19:07:01 +0300

    add navigation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* ========== Navigation =========== */
+const navList = document.querySelector(".nav-list");
+
+document.querySelector(".hamburger").onclick = () => {
+  navList.classList.add("show");
+};
+
+document.querySelector(".close").onclick = () => {
+  navList.classList.remove("show");
+};
