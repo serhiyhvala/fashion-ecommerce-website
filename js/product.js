63cddca03e0b4e3147a5fc6be748eec5592d84ab
Your Name <you@example.com>
2022-03-31 18:41:20 +0300

    add display and filter product

--- js/product.js
+++ js/product.js
@@ -21,21 +21,79 @@ const swiper = new Swiper('.mySwiper', {
 
 const getProducts = async () => {
     try {
-      const results = await fetch('./data/products.json');
-      const data = await results.json();
-      const products = data.products;
-      return products;
+        const results = await fetch('./data/products.json');
+        const data = await results.json();
+        const products = data.products;
+        return products;
     } catch (err) {
-      console.log(err);
+        console.log(err);
     }
-  };
-  
-  const ProductsWrapper = document.getElementById('products');
-  
-  window.addEventListener('DOMContentLoaded', async function () {
+};
+
+const ProductsWrapper = document.getElementById('products');
+
+window.addEventListener('DOMContentLoaded', async function () {
     let products = await getProducts();
     products = products.filter((product) => product.category === 'Dresses');
     displayProductItems(products);
     loadData();
-  });
-  
+});
+
+/* ========== Display Products =========== */
+const displayProductItems = (items) => {
+    let displayProduct = items.map(
+        (product) => `
+                  <div class="swiper-slide">
+                <div class="product">
+                  <div class="top d-flex">
+                    <img src=${product.url} alt="" />
+                    <div class="icon d-flex">
+                      <i class="bx bxs-heart"></i>
+                    </div>
+                  </div>
+                  <div class="bottom">
+                    <h4>${product.title}</h4>
+                    <div class="d-flex">
+                      <div class="price">$${product.price}</div>
+                      <div class="rating">
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                    `
+    );
+
+    displayProduct = displayProduct.join('');
+    ProductsWrapper.innerHTML = displayProduct;
+};
+
+/* ========== Filter Products =========== */
+const filters = [...document.querySelectorAll('.filters div')];
+
+filters.forEach((filter) => {
+    filters[2].classList.add('active');
+    filter.addEventListener('click', async (e) => {
+        const id = e.target.getAttribute('data-filter');
+        const target = e.target;
+        const products = await getProducts();
+        filters.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+        target.classList.add('active');
+
+        let menuCategory = products.filter((product) => {
+            if (product.category === id) {
+                return product;
+            }
+        });
+
+        displayProductItems(menuCategory);
+        swiper.update();
+    });
+});
